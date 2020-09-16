Fixed more masking bugs

nmrgnn/losses.py
@@ -18,16 +18,14 @@ def corr_coeff(x, y, w = None):
     cor = tf.math.divide_no_nan(cov, m * tf.math.sqrt((xm2 - xm**2) * (ym2 - ym**2)))
     return cor
 
-def corr_loss(labels, predictions, sample_weight = None, s=1e-3):
+def corr_loss(labels, predictions, sample_weight = None, s=1e-2):
     '''
     Mostly correlation, with small squared diff
     '''
     x = predictions
     y = labels[:,0]
-    if sample_weight is None:
-        # make it non-zero labels
-        sample_weight = tf.cast(tf.math.abs(y) > 1e-10, tf.float32)
-    l2 = tf.math.divide_no_nan(tf.reduce_sum( sample_weight * tf.math.abs( y - x) ), tf.reduce_sum(sample_weight))
-    return l2# + (1 - corr_coeff(x, y, sample_weight))
+    w = labels[:,-1]
+    l2 = tf.math.divide_no_nan(tf.reduce_sum( w * tf.math.abs( y - x) ), tf.reduce_sum(w))
+    return s * l2 + (1 - corr_coeff(x, y, w)**2)
 
 

nmrgnn/main.py
@@ -53,6 +53,18 @@ def load_data(tfrecords, validation, embeddings, scale=False):
     return train_data, validation_data
 
 
+def setup_optimizations():
+    #tf.debugging.enable_check_numerics()
+
+    tf.config.optimizer.set_jit(True)
+    from tensorflow.keras.mixed_precision import experimental as mixed_precision
+
+    #policy = mixed_precision.Policy('mixed_float16')
+    #mixed_precision.set_policy(policy)
+
+
+
+
 @main.command()
 @click.argument('tfrecords')
 @click.argument('epochs', default=3)
@@ -64,18 +76,14 @@ def load_data(tfrecords, validation, embeddings, scale=False):
 def train(tfrecords, epochs, embeddings, validation, checkpoint_path, tensorboard, load):
     '''Train the model'''
 
-    #tf.debugging.enable_check_numerics()
 
-
-    strategy = tf.distribute.MirroredStrategy()
-    with strategy.scope():
-        model = nmrgnn.build_GNNModel()
-        if load:
-            model.load_weights(checkpoint_path)
+    model = nmrgnn.build_GNNModel()
+    if load:
+        model.load_weights(checkpoint_path)
     callbacks = []
     # set-up learning rate scheduler
     reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5,
-                                                     patience=10, min_lr=1e-6, verbose=1)
+                                                     patience=4, min_lr=1e-6, verbose=1)
     callbacks.append(reduce_lr)
     # tensorboard
     if tensorboard is not None:
@@ -104,6 +112,8 @@ def train(tfrecords, epochs, embeddings, validation, checkpoint_path, tensorboar
 def eval_tfrecords(tfrecords, checkpoint, validation, output):
     '''Evaluate specific file'''    
     
+    setup_optimizations()
+
     model = nmrgnn.build_GNNModel(metrics=False)
     model.load_weights(checkpoint)
     train_data, validation_data = load_data(tfrecords, validation, None)
@@ -151,6 +161,8 @@ def eval_tfrecords(tfrecords, checkpoint, validation, output):
 @click.option('--tensorboard', default=None, help='path to tensorboard logs')
 def hyper(tfrecords, epochs, embeddings, tuning_path, validation, tensorboard):
     '''Tune hyperparameters the model'''
+
+    setup_optimizations()
 
     callbacks = []
     # set-up learning rate scheduler

nmrgnn/metrics.py
@@ -35,16 +35,9 @@ class NameMAE(tf.keras.metrics.Metric):
 
     def update_state(self, y_true, y_pred, sample_weight=None):
         # mask diff by which predictions match the label
-        mask = tf.cast(tf.reduce_any(
+        mask = y_true[:, -1] * tf.cast(tf.reduce_any(
             tf.equal(tf.cast(y_true[:, 1][..., tf.newaxis], self.ln.dtype), self.ln), axis=-1), tf.float32)
         diff = tf.math.abs(y_true[:, 0] - y_pred) * mask
-        if sample_weight is None:
-            # make it non-zero labels
-            sample_weight = tf.cast(tf.math.abs(y_true[:,0]) > 1e-10, tf.float32)
-
-        mask *= sample_weight
-        diff *= sample_weight
-            
         N = tf.reduce_sum(mask)
         self.mae.assign(tf.math.divide_no_nan(tf.reduce_sum(diff), N))
 
@@ -71,13 +64,9 @@ class NameR2(tf.keras.metrics.Metric):
 
     def update_state(self, y_true, y_pred, sample_weight=None):
         # mask diff by which predictions match the label
-        mask = tf.cast(tf.reduce_any(
+        mask = y_true[:, -1] * tf.cast(tf.reduce_any(
             tf.equal(tf.cast(y_true[:, 1][..., tf.newaxis], self.ln.dtype), self.ln), axis=-1), tf.float32)
-        if sample_weight is None:
-            # make it non-zero labels
-            sample_weight = tf.cast(tf.math.abs(y_true[:,0]) > 1e-10, tf.float32)
-        mask *= sample_weight
-        r2 = NameR2.corr_coeff(y_true[:, 0], y_pred, mask)
+        r2 = NameR2.corr_coeff(y_true[:, 0], y_pred, mask)**2
         self.r2.assign(r2)
 
     def result(self):
@@ -95,6 +84,6 @@ class NameR2(tf.keras.metrics.Metric):
         ym = tf.reduce_sum(w * y) / m
         xm2 = tf.reduce_sum(w * x**2) / m
         ym2 = tf.reduce_sum(w * y**2) / m
-        cov = tf.reduce_sum( w * (x - xm) * (y - ym) ) / m
-        cor = tf.math.divide_no_nan(cov, tf.math.sqrt((xm2 - xm**2) * (ym2 - ym**2)))
+        cov = tf.reduce_sum( w * (x - xm) * (y - ym) )
+        cor = tf.math.divide_no_nan(cov, m * tf.math.sqrt((xm2 - xm**2) * (ym2 - ym**2)))
         return cor

nmrgnn/model.py
@@ -8,17 +8,16 @@ from .layers import *
 from .losses import *
 from .metrics import *
 
-# An object stores model hyper parameters
-
 
 def build_GNNModel(hp=kt.HyperParameters(), metrics=True):
-    hp.Choice('atom_feature_size', [32, 64, 128, 256], ordered=True, default=64)
-    hp.Choice('edge_feature_size', [1, 2, 4, 8, 16, 32], ordered=True, default=16)
+    '''Build model with hyper parameter object'''
+    hp.Choice('atom_feature_size', [32, 64, 128, 256], ordered=True, default=128)
+    hp.Choice('edge_feature_size', [1, 2, 4, 8, 16], ordered=True, default=4)
     hp.Choice('edge_hidden_size', [16, 32, 64, 128, 256], ordered=True, default=128)
     hp.Int('mp_layers', 1, 6, step=1, default=4)
     hp.Int('fc_layers', 2, 6, step=1, default=3)
     hp.Int('edge_fc_layers', 2, 6, step=1, default=4)
-    hp.Choice('noise', [0.0, 0.005, 0.01, 0.02, 0.05], ordered=True, default=0.02)
+    hp.Choice('noise', [0.0, 0.025, 0.05, 0.1], ordered=True, default=0.025)
     hp.Fixed('rbf_low', 0.005)
     hp.Fixed('rbf_high', 0.15)
     hp.Choice('mp_activation', [
@@ -33,7 +32,7 @@ def build_GNNModel(hp=kt.HyperParameters(), metrics=True):
 
     # compile with MSLE (to treat vastly different label mags)
     optimizer = tf.keras.optimizers.Adam(
-        hp.Choice('learning_rate', [1e-2, 1e-3, 5e-3, 1e-4, 1e-5], default=1e-4))
+        hp.Choice('learning_rate', [1e-2, 1e-3, 1e-4, 1e-5], default=1e-3))
     loss = corr_loss
     embeddings = nmrdata.load_embeddings()
     label_idx = type_mask(r'.*\-H.*', embeddings, regex=True)
@@ -52,9 +51,9 @@ def build_GNNModel(hp=kt.HyperParameters(), metrics=True):
     n_r2 = NameR2(label_idx, name='n_r2')
     label_idx = type_mask(r'.*\-C.*', embeddings, regex=True)
     c_r2 = NameR2(label_idx, name='c_r2')
-    label_idx = type_mask(r'.*\-H', embeddings, regex=True)
+    label_idx = type_mask(r'.*\-H$', embeddings, regex=True)
     hn_r2 = NameR2(label_idx, name='hn_r2')
-    label_idx = type_mask(r'.*\-HA*', embeddings, regex=True)
+    label_idx = type_mask(r'.*\-HA.*', embeddings, regex=True)
     ha_r2 = NameR2(label_idx, name='ha_r2')
     model.compile(optimizer=optimizer,
                   loss=loss,
@@ -80,9 +79,17 @@ def build_GNNModel(hp=kt.HyperParameters(), metrics=True):
 class EdgeFCBlock(keras.layers.Layer):
     def __init__(self, hypers):
         super(EdgeFCBlock, self).__init__(name='edge-fc-block')
-        self.edge_fc = []
+        
+        # add l1 regularizer to 
+        # input, so that we 
+        # zero-out unused distance features
+        self.edge_fc = [keras.layers.Dense(
+            hypers.get('edge_hidden_size'), 
+            activation=hypers.get('fc_activation'),
+            kernel_regularizer='l1'
+        )]
         # stack Dense Layers as a block
-        for _ in range(hypers.get('edge_fc_layers') - 1):
+        for _ in range(hypers.get('edge_fc_layers') - 2):
             self.edge_fc.append(keras.layers.Dense(
                 hypers.get('edge_hidden_size'), activation=hypers.get('fc_activation')))
         # activation function for the last layer is 'tanh'